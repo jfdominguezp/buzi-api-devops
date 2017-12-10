0d2707f72ef3a74d9fdcee39ab3ffbd3bb699e3b
Juan Fernando Domínguez <juan@maarket.co>
2017-12-10 15:36:44 -0500

    Get coupons by userId

--- api/routes/business-routes.js
+++ api/routes/business-routes.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var Business = require('../models/business');
+var Coupon = require('../models/coupon');
 var router = express.Router();
 
 router.post('/', businessPost);
+router.get('/:userId/coupons', getBusinessCoupons);
 
 function businessPost(request, response) {
   var body = request.body;
@@ -25,6 +27,21 @@ function businessPost(request, response) {
     }else{
       response.status(200).json(data);
     }
+  });
+
+}
+
+function getBusinessCoupons(request, response) {
+  var businessId = request.params.userId;
+  if(!businessId) return response.status(400).json('Bad Request');
+
+  Business.findOne({ userId: businessId }, 'shortId', function(error, business){
+    if(error) return response.status(500).json('Error');
+    if(!business || !business.shortId) return response.status(404).json('The specified business does not exist');
+    Coupon.find({ businessId: business.shortId }, function(error, coupons) {
+      if(error) return response.status(500).json('Error');
+      return response.status(200).json(coupons);
+    });
   });
 
 }
