207b1d56b8c85407e508cfc350b2691a7202e7d8
Juan Domínguez <juan@buziapp.com>
2018-07-14 12:59:45 -0500

    Configuration changes, ESLint installed and packages updated

--- config/server-config.js
+++ config/server-config.js
@@ -1,19 +1,18 @@
-var config = { };
-
-config.mongoURI = Object.freeze({
-    development: 'mongodb://api:8JcCzT71vSuiLNmd@production-shard-00-00-m0khv.mongodb.net:27017,production-shard-00-01-m0khv.mongodb.net:27017,production-shard-00-02-m0khv.mongodb.net:27017/buzi_dev?ssl=true&replicaSet=Production-shard-0&authSource=admin',
-    test: 'mongodb://api:8JcCzT71vSuiLNmd@production-shard-00-00-m0khv.mongodb.net:27017,production-shard-00-01-m0khv.mongodb.net:27017,production-shard-00-02-m0khv.mongodb.net:27017/buzi_dev?ssl=true&replicaSet=Production-shard-0&authSource=admin',
-    production: process.env.MONGODB_CONNECTION_STRING
-});
-
-config.authConfig = Object.freeze({
-    //jwtSecret: 's3cr3tK3Yf0rMrCup0nS1nc323012018',
-    jwtSecret: process.env.JWT_SECRET,
-    //refreshSecret: 's3cr3tF0rR3fr35h70k3nsS1nc323012018',
-    refreshSecret: process.env.REFRESH_SECRET,
-    //issuer: 'auth.mrcupon.co'
-    issuer: process.env.JWT_ISSUER
-});
+const config = {
+    mongoURI: {
+        development: 'mongodb://api:8JcCzT71vSuiLNmd@production-shard-00-00-m0khv.mongodb.net:27017,production-shard-00-01-m0khv.mongodb.net:27017,production-shard-00-02-m0khv.mongodb.net:27017/buzi_dev?ssl=true&replicaSet=Production-shard-0&authSource=admin',
+        test: 'mongodb://api:8JcCzT71vSuiLNmd@production-shard-00-00-m0khv.mongodb.net:27017,production-shard-00-01-m0khv.mongodb.net:27017,production-shard-00-02-m0khv.mongodb.net:27017/buzi_dev?ssl=true&replicaSet=Production-shard-0&authSource=admin',
+        production: process.env.MONGODB_CONNECTION_STRING
+    },
+    authConfig: {
+        //jwtSecret: 's3cr3tK3Yf0rMrCup0nS1nc323012018',
+        jwtSecret: process.env.JWT_SECRET,
+        //refreshSecret: 's3cr3tF0rR3fr35h70k3nsS1nc323012018',
+        refreshSecret: process.env.REFRESH_SECRET,
+        //issuer: 'auth.mrcupon.co'
+        issuer: process.env.JWT_ISSUER
+    },
+};
 
 module.exports = config;
 
